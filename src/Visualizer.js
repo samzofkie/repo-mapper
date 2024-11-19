f3bca9f8e8a42ef06af50a57738b060e353ce9af
Sam Zofkie <samzofkie@gmail.com>
2024-11-19 10:18:49 -0600

    Implemented fetching logic into Visualizer.loadRepo().

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -32,48 +32,43 @@ export class Visualizer {
   async loadRepo() {
     this.repoAttempter.showLoading();
 
-    let path;
+    let owner, name, defaultBranch, flatTree;
     try {
-      path = this.parsePath();
+      [owner, name] = Visualizer.parsePath();
+      defaultBranch = await Visualizer.getRepoDefaultBranch(owner, name);
+
+      owner = encodeURIComponent(owner);
+      name = encodeURIComponent(name);
+      defaultBranch = encodeURIComponent(defaultBranch);
+
+      flatTree = await Visualizer.getGitTree(owner, name, defaultBranch);
+      console.log(flatTree);
+
     } catch (error) {
-      if (error instanceof VisualizerPathError) {
+      if (
+        error instanceof VisualizerPathError ||
+        error instanceof VisualizerRequestError
+      ) {
         this.repoAttempter.showError(error.message);
         return;
       } else
         throw error;
     }
-
-    this.owner = path[0];
-    this.name = path[1];
-
-    let defaultBranch;
-    try {
-      defaultBranch = await this.getRepoDefaultBranch();
-    } catch (error) {
-      if (error instanceof VisualizerRequestError) {
-        this.repoAttempter.showError(error.message);
-        return;
-      } else
-        throw error;
-    }
-
-    console.log(this.owner, this.name, defaultBranch);
-
   }
 
-  parsePath() {
+  static parsePath() {
     const path = window.location.pathname.split('/').slice(1);
     if (path.length !== 2)
       throw new VisualizerPathError('URL endpoint should be of the form /[owner]/[repo]!');
     return path;
   }
 
-  async getRepoDefaultBranch() {
+  static async getRepoDefaultBranch(owner, name) {
     const searchParams = new URLSearchParams(window.location.search);
     if (searchParams.has('defaultBranch'))
       return searchParams.get('defaultBranch');
 
-    const url = `https://api.github.com/repos/${this.owner}/${this.name}`;
+    const url = `https://api.github.com/repos/${owner}/${name}`;
     console.log(`GET ${url}`);
     const res = await fetch(url);
 
@@ -87,5 +82,21 @@ export class Visualizer {
       return json.default_branch;
     }
   }
-  
+
+  static async getGitTree(owner, name, defaultBranch) {
+    const url = `https://api.github.com/repos/${owner}/${name}/git/trees/${defaultBranch}?recursive=true`;
+    console.log(`GET ${url}`);
+    const res = await fetch(url);
+
+    if (res.status !== 200) {
+      throw new VisualizerRequestError(
+        ErrorLogger.httpsError(res.url, res.status)
+      );
+
+    } else {
+      const json = await res.json();
+      return json.tree;
+    }
+  }
+
 }
