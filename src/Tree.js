19dc1c9b1088611945776004e2d0528e7858f5cf
Sam Zofkie <samzofkie@gmail.com>
2024-11-20 10:50:02 -0600

    Refactored repo-tree fetching and parsing into new Tree class.

---
+++ src/Tree.js
@@ -0,0 +1,111 @@
+import { ErrorLogger } from './ErrorLogger';
+
+export class TreePathError extends Error {}
+export class TreeRequestError extends Error {}
+
+export class Tree {
+  static async loadTree() {
+    let [owner, name] = Tree.parsePath();
+    let defaultBranch = await Tree.getRepoDefaultBranch(owner, name);
+
+    owner = encodeURIComponent(owner);
+    name = encodeURIComponent(name);
+    defaultBranch = encodeURIComponent(defaultBranch);
+
+    let flatTree = await Tree.getGitTree(owner, name, defaultBranch);
+    return Tree.buildTreeFromFlat(flatTree);
+  }
+
+  static parsePath() {
+    const path = window.location.pathname.split('/').slice(1);
+    if (path.length !== 2)
+      throw new TreePathError('URL endpoint should be of the form /[owner]/[repo]!');
+    return path;
+  }
+
+  static async getRepoDefaultBranch(owner, name) {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('defaultBranch'))
+      return searchParams.get('defaultBranch');
+
+    const url = `https://api.github.com/repos/${owner}/${name}`;
+    console.log(`GET ${url}`);
+    const res = await fetch(url);
+
+    if (res.status !== 200) {
+      throw new TreeRequestError(
+        ErrorLogger.httpsError(res.url, res.status)
+      );
+      
+    } else {
+      const json = await res.json();
+      return json.default_branch;
+    }
+  }
+
+  static async getGitTree(owner, name, defaultBranch) {
+    const url = `https://api.github.com/repos/${owner}/${name}/git/trees/${defaultBranch}?recursive=true`;
+    console.log(`GET ${url}`);
+    const res = await fetch(url);
+
+    if (res.status !== 200) {
+      throw new TreeRequestError(
+        ErrorLogger.httpsError(res.url, res.status)
+      );
+
+    } else {
+      const json = await res.json();
+      return json.tree;
+    }
+  }
+
+  static calculateTreeSizes(tree) {
+    for (let entry of tree.entries) {
+      if (entry.type === 'tree')
+        Tree.calculateTreeSizes(entry);
+      tree.size += entry.size;
+    }
+  }
+
+  static buildTreeFromFlat(flatTree) {
+    const tree = {
+      type: 'tree',
+      size: 0,
+      entries: [],
+    };
+
+    for (const entry of flatTree) {
+      const path = entry.path.split('/').slice(0, -1);
+      const [name] = entry.path.split('/').slice(-1);
+
+      // Navigate to spot in path
+      let spot = tree;
+      for (let stop of path)
+        spot = spot.entries.find(entry => entry.name === stop);
+      
+      // Insert info at spot
+      if (entry.type === 'tree') {
+        spot.entries.push({
+          name: name,
+          type: 'tree',
+          size: 0,
+          entries: [],
+        });
+      } else if (entry.type === 'blob') {
+        spot.entries.push({
+          name: name,
+          type: 'blob',
+          size: entry.size,
+        });
+      } else {
+        //throw new Error(`Tree.buildTreeFromFlat() encountered unexpected entry type: "${entry.type}"`);
+      }
+    }
+
+    // Do a DFS to calculate cumulative sizes of trees based on blobs (modifies
+    // tree in-place)
+    Tree.calculateTreeSizes(tree);
+
+    return tree;
+  }
+}
--- src/Visualizer.js
+++ src/Visualizer.js
@@ -1,8 +1,6 @@
+import { Tree, TreePathError, TreeRequestError } from './Tree';
 import { Attempter } from './Attempter';
-import { ErrorLogger } from './ErrorLogger';
 
-class VisualizerPathError extends Error {}
-class VisualizerRequestError extends Error {}
 
 // The main *init* method is `loadRepo()`.
 export class Visualizer {
@@ -10,6 +8,8 @@ export class Visualizer {
   static SCALAR_PRECISION = 3;
 
   constructor() {
+    this.tree = new Tree;
+
     this.main = document.querySelector('#visualizer');
     this.result = document.querySelector('#visualizer #repo');
 
@@ -37,19 +37,11 @@ export class Visualizer {
     this.repoAttempter.showLoading();
 
     try {
-      let [owner, name] = Visualizer.parsePath();
-      let defaultBranch = await Visualizer.getRepoDefaultBranch(owner, name);
-
-      owner = encodeURIComponent(owner);
-      name = encodeURIComponent(name);
-      defaultBranch = encodeURIComponent(defaultBranch);
-
-      let flatTree = await Visualizer.getGitTree(owner, name, defaultBranch);
-      this.tree = Visualizer.buildTreeFromFlat(flatTree);
+      this.tree = await Tree.loadTree();
     } catch (error) {
       if (
-        error instanceof VisualizerPathError ||
-        error instanceof VisualizerRequestError
+        error instanceof TreePathError ||
+        error instanceof TreeRequestError
       ) {
         this.repoAttempter.showError(error.message);
         return;
@@ -73,99 +65,6 @@ export class Visualizer {
     );
     this.result.append(...listItemNodes);
     this.repoAttempter.showResult();
-  }
-
-  static parsePath() {
-    const path = window.location.pathname.split('/').slice(1);
-    if (path.length !== 2)
-      throw new VisualizerPathError('URL endpoint should be of the form /[owner]/[repo]!');
-    return path;
-  }
-
-  static async getRepoDefaultBranch(owner, name) {
-    const searchParams = new URLSearchParams(window.location.search);
-    if (searchParams.has('defaultBranch'))
-      return searchParams.get('defaultBranch');
-
-    const url = `https://api.github.com/repos/${owner}/${name}`;
-    console.log(`GET ${url}`);
-    const res = await fetch(url);
-
-    if (res.status !== 200) {
-      throw new VisualizerRequestError(
-        ErrorLogger.httpsError(res.url, res.status)
-      );
-      
-    } else {
-      const json = await res.json();
-      return json.default_branch;
-    }
-  }
-
-  static async getGitTree(owner, name, defaultBranch) {
-    const url = `https://api.github.com/repos/${owner}/${name}/git/trees/${defaultBranch}?recursive=true`;
-    console.log(`GET ${url}`);
-    const res = await fetch(url);
-
-    if (res.status !== 200) {
-      throw new VisualizerRequestError(
-        ErrorLogger.httpsError(res.url, res.status)
-      );
-
-    } else {
-      const json = await res.json();
-      return json.tree;
-    }
-  }
-
-  static calculateTreeSizes(tree) {
-    for (let entry of tree.entries) {
-      if (entry.type === 'tree')
-        Visualizer.calculateTreeSizes(entry);
-      tree.size += entry.size;
-    }
-  }
-
-  static buildTreeFromFlat(flatTree) {
-    const tree = {
-      type: 'tree',
-      size: 0,
-      entries: [],
-    };
-
-    for (const entry of flatTree) {
-      const path = entry.path.split('/').slice(0, -1);
-      const [name] = entry.path.split('/').slice(-1);
-
-      // Navigate to spot in path
-      let spot = tree;
-      for (let stop of path)
-        spot = spot.entries.find(entry => entry.name === stop);
-      
-      // Insert info at spot
-      if (entry.type === 'tree') {
-        spot.entries.push({
-          name: name,
-          type: 'tree',
-          size: 0,
-          entries: [],
-        });
-      } else if (entry.type === 'blob') {
-        spot.entries.push({
-          name: name,
-          type: 'blob',
-          size: entry.size,
-        });
-      } else {
-        throw new Error(`Visualizer.buildTreeFromFlat() encountered unexpected entry type: "${entry.type}"`);
-      }
-    }
-
-    // Do a DFS to calculate cumulative sizes of trees based on blobs (modifies
-    // tree in-place)
-    Visualizer.calculateTreeSizes(tree);
-
-    return tree;
   }
 
   static getWindowSize() {
