e050023e2a3aadff55b0f286c426ce8112e8c5e4
Sam Zofkie <samzofkie@gmail.com>
2024-11-25 13:01:53 -0600

    Added max and min sizes to BlobGaggle.

--- src/BlobGaggle.js
+++ src/BlobGaggle.js
@@ -1,6 +1,9 @@
 import { Circle } from './Circle';
 
 export class BlobGaggle {
+  static MAX_SIZE = 200;
+  static MIN_SIZE = 10;
+
   constructor(tree) {
     this.blobs = tree.entries.filter(entry => entry.type === 'blob');
   }
@@ -11,9 +14,15 @@ export class BlobGaggle {
 
   scaleBlobs(scalar) {
     const blobs = this.getCopyOfBlobs();
+    const scale = blob => {
+      let size = scalar * Math.sqrt(blob.bytes);
+      size = Math.min(size, BlobGaggle.MAX_SIZE);
+      size = Math.max(size, BlobGaggle.MIN_SIZE);
+      return size;
+    };
     return blobs.map(blob => ({
       ...blob,
-      size: scalar * Math.sqrt(blob.bytes),
+      size: scale(blob),
     }));
   }
 
@@ -25,14 +34,20 @@ export class BlobGaggle {
   calculateScalarAndRows(diameter) {
     let precision = 5;
     let scalar=0, blobs, solution;
-    for (let i=0; i<precision; i++) {
+
+    for (let i=-100; i<precision; i++) {
       let inc = 10**-i;
       blobs = this.scaleBlobs(scalar + inc);
       solution = Circle.packIntoRows(diameter, blobs.map(blob => blob.size));
+      let iters = 0, MAX_ITERS = 10;
       while(solution !== null) {
         scalar += inc;
         blobs = this.scaleBlobs(scalar + inc);
         solution = Circle.packIntoRows(diameter, blobs.map(blob => blob.size));
+        if (++iters > MAX_ITERS) {
+          console.error('BlobGaggle.calculateScalarAndRows() hit MAX_ITERS!');
+          break;
+        }
       }
     }
     blobs = this.scaleBlobs(scalar);
